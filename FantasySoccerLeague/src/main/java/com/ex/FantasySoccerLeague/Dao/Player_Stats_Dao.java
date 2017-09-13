556787c3f57a9d7a8f930189cfe84f5952909755
Vincent Bolden <Vincent.B27@outlook.com>
2017-09-13 08:38:21 -0400

    Fixed Error in Player_Stats_Dao

---
+++ FantasySoccerLeague/src/main/java/com/ex/FantasySoccerLeague/Dao/Player_Stats_Dao.java
@@ -0,0 +1,10 @@
+package com.ex.FantasySoccerLeague.Dao;
+
+
+import com.ex.FantasySoccerLeague.tables.Player_Stats;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Player_Stats_Dao extends JpaRepository<Player_Stats, Integer> {
+}
