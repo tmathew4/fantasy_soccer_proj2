e0b32adc12de4398a4da1e004585c8de382fb682
Theara Ya <theara_ya@my.uri.edu>
2017-09-18 12:58:20 -0400

    change

--- FantasySoccerLeague/src/main/java/com/ex/FantasySoccerLeague/Dao/Position_Dao.java
+++
@@ -1,9 +0,0 @@
-package com.ex.FantasySoccerLeague.Dao;
-
-import com.ex.FantasySoccerLeague.tables.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Position_Dao extends JpaRepository<Position, Integer> {
-}
